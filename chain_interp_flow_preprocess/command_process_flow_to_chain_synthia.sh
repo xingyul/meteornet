1d2173e21ac40783925053b0313d846daefaa4b7
Xingyu Liu <liuxy610042@gmail.com>
2020-02-17 08:14:37 -0800

    deleted clutter

--- chain_interp_flow_preprocess/command_process_flow_to_chain_synthia.sh
+++ chain_interp_flow_preprocess/command_process_flow_to_chain_synthia.sh
@@ -1,15 +1,6 @@
 
 
 
-parser.add_argument('--original_pc_dir', default='../semantic_seg_synthia/processed_pc', help='Original point cloud directory [default: ../
-semantic_seg_synthia/processed_pc]')
-parser.add_argument('--pairwise_flow_dir', default='../semantic_seg_synthia/init_flow', help='Initial scene flow directory [default: ../
-semantic_seg_synthia/init_flow]')
-parser.add_argument('--chained_flowed_dir', default='../semantic_seg_synthia/chained_flow', help='Chained scene flow directory, output
-[default: ../semantic_seg_synthia/chained_flow]')
-parser.add_argument('--maximum_frame_diff', type=int, default=3, help='Maximum frame difference [default: 3]')
-parser.add_argument('--num_threads', type=int, default=4, help='Number of threads [default: 3]')
-
 
 original_pc_dir=../semantic_seg_synthia/processed_pc
 pairwise_flow_dir=../semantic_seg_synthia/init_flow
